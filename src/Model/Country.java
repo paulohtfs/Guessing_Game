9b011ab3d6b32fe14348fac11d25f09445590383
paulohtfs <paulohtfs@gmail.com>
2013-09-15 09:37:14 -0300

    Implements methods interface Tips

--- src/Model/Country.java
+++ src/Model/Country.java
@@ -6,4 +6,22 @@ public class Country extends Category{
 		// TODO Auto-generated constructor stub
 	}
 
+	@Override
+	public void addsTips(String tips) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public String[] getTips() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void setTips(String[] tips) {
+		// TODO Auto-generated method stub
+		
+	}
+
 }
